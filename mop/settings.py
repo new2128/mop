7dad405810026202481ea347bf419365d08e7dde
Lindy Lindstrom <llindstrom@lco.global>
2020-05-08 21:20:24 +0000

    make settings DEBUG respond to env var

--- mop/settings.py
+++ mop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY','+xg%_n7l==$@v7))t1&amp;@a_tl)qo$=g7rl^wp7bx^qu+g7+hiaj')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
