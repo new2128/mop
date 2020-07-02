6a2ac69944b4b5303c3a0f6012d6b8934b3e23bd
Etienne Bachelet <etibachelet@gmail.com>
2020-07-02 21:17:12 +0200

    Fixing OMEGA users

--- mop/management/commands/open_targets_to_OMEGA_users.py
+++ mop/management/commands/open_targets_to_OMEGA_users.py
@@ -9,16 +9,16 @@ from tom_targets.models import Target
 class Command(BaseCommand):
 
     help = 'Add targets to the OMEGA users list'
-   # def add_arguments(self, parser):
-   #     parser.add_argument('target_name', help='name of the event to compute errors')
+    # def add_arguments(self, parser):
+    #     parser.add_argument('target_name', help='name of the event to compute errors')
 
-    
+
     def handle(self, *args, **options):
-    
-   	omega_group = Group.objects.filter(name='OMEGA').first()
-   	
-	for target in Target.objects.all():
-	
-	    assign_perm('tom_targets.view_target', omega_group, target)
-	    assign_perm('tom_targets.change_target', omega_group, target)
-	    #assign_perm('tom_targets.delete_target', omega_group, t)
+
+        omega_group = Group.objects.filter(name='OMEGA').first()
+
+        for target in Target.objects.all():
+
+            assign_perm('tom_targets.view_target', omega_group, target)
+            assign_perm('tom_targets.change_target', omega_group, target)
+            #assign_perm('tom_targets.delete_target', omega_group, t)
