ef30253c1d04be243707da553e4ba08397523ada
Etienne Bachelet <etibachelet@gmail.com>
2020-07-03 02:23:17 +0200

    Add custom data products**6

--- mop/settings.py
+++ mop/settings.py
@@ -199,6 +199,16 @@ CACHES = {
     }
 }
 
+#Using aws
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRECT_ACCESS_KEY', '')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', '')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', '')
+AWS_DEFAULT_ACL = None
+
 # TOM Specific configuration
 TARGET_TYPE = 'SIDEREAL'
 
