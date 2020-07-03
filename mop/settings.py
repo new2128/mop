872a26bb2eb0a9a59934ae2d43d479ed25ad01d3
Etienne Bachelet <etibachelet@gmail.com>
2020-07-03 02:30:04 +0200

    Add custom data products**7

--- mop/settings.py
+++ mop/settings.py
@@ -200,8 +200,6 @@ CACHES = {
 }
 
 #Using aws
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRECT_ACCESS_KEY', '')
