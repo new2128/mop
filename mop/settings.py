20269874e0e8d60b4fb35fb99e2c6c5cf795e5ab
Etienne Bachelet <etibachelet@gmail.com>
2020-07-03 02:05:26 +0200

    Add custom data products**5

--- mop/settings.py
+++ mop/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import ast
 import tempfile
-from mop.toolbox import custom_dataproducts
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
