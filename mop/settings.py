1bc07ee70182018b172e680cb18bc3044ae6a88a
Etienne Bachelet <etibachelet@gmail.com>
2020-07-17 20:02:34 +0200

    Custom Photometry Processor

--- mop/settings.py
+++ mop/settings.py
@@ -259,7 +259,7 @@ DATA_PRODUCT_TYPES = {
 }
 
 DATA_PROCESSORS = {
-    'photometry': 'tom_dataproducts.processors.photometry_processor.PhotometryProcessor',
+    'photometry': 'mop.processors.photometry_processor.PhotometryProcessor',
     'spectroscopy': 'tom_dataproducts.processors.spectroscopy_processor.SpectroscopyProcessor',
 }
 
