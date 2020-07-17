39e487e318c2119b584af300749b156b894aa9ac
Etienne Bachelet <etibachelet@gmail.com>
2020-07-18 01:03:30 +0200

    Set MOA harvest to 2 years of baseline

--- mop/brokers/moa.py
+++ mop/brokers/moa.py
@@ -98,7 +98,7 @@ class MOABroker(GenericBroker):
                     tot_flux = float(self.event_dictionnary[target.name][2])+float(phot[1])
                     mag = float(self.event_dictionnary[target.name][1])-2.5*np.log10(tot_flux)
                     emag = float(phot[2])/tot_flux*2.5/np.log(10)
-                    if (np.isfinite(mag)) & (emag>0) & (emag<0.25) & (float(phot[0])>time_now-5*365.25): #Harvest the last 5 years 
+                    if (np.isfinite(mag)) & (emag>0) & (emag<0.25) & (float(phot[0])>time_now-2*365.25): #Harvest the last 5 years 
                         jd.append(float(phot[0]))
                         mags.append(mag)
                         emags.append(emag)
