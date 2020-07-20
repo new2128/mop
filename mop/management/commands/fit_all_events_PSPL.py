0e423fb38e7882e4ca0dfc8a4da18fca56aab730
Etienne Bachelet <etibachelet@gmail.com>
2020-07-20 21:11:21 +0200

    Fix fit all events cron jobs

--- mop/management/commands/fit_all_events_PSPL.py
+++ mop/management/commands/fit_all_events_PSPL.py
@@ -47,31 +47,32 @@ class Command(BaseCommand):
 
                extras = {'Alive':alive}
                target.save(extras = extras)
-               return
-
-           datasets = ReducedDatum.objects.filter(target=target)
-           time = [Time(i.timestamp).jd for i in datasets if i.data_type == 'photometry']
-            
-           phot = []
-           for data in datasets:
-               if data.data_type == 'photometry':
-                  try:
-                       phot.append([json.loads(data.value)['magnitude'],json.loads(data.value)['error'],json.loads(data.value)['filter']])
            
-                  except:
-                       # Weights == 1
-                       phot.append([json.loads(data.value)['magnitude'],1,json.loads(data.value)['filter']])
+           else:
+
+               datasets = ReducedDatum.objects.filter(target=target)
+               time = [Time(i.timestamp).jd for i in datasets if i.data_type == 'photometry']
+            
+               phot = []
+               for data in datasets:
+                   if data.data_type == 'photometry':
+                      try:
+                           phot.append([json.loads(data.value)['magnitude'],json.loads(data.value)['error'],json.loads(data.value)['filter']])
+           
+                      except:
+                           # Weights == 1
+                           phot.append([json.loads(data.value)['magnitude'],1,json.loads(data.value)['filter']])
                
 
-           photometry = np.c_[time,phot]
+               photometry = np.c_[time,phot]
 
-           t0_fit,u0_fit,tE_fit,piEN_fit,piEE_fit,mag_source_fit,mag_blend_fit,mag_baseline_fit,cov,model = fittools.fit_PSPL_parallax(target.ra, target.dec, photometry)
-           #Add photometry model
-           data = {'lc_model_time': model.lightcurve_magnitude[:,0].tolist(),
+               t0_fit,u0_fit,tE_fit,piEN_fit,piEE_fit,mag_source_fit,mag_blend_fit,mag_baseline_fit,cov,model = fittools.fit_PSPL_parallax(target.ra, target.dec, photometry)
+               #Add photometry model
+               data = {'lc_model_time': model.lightcurve_magnitude[:,0].tolist(),
                'lc_model_magnitude': model.lightcurve_magnitude[:,1].tolist()
                         }
 
-           rd, created = ReducedDatum.objects.get_or_create(
+               rd, created = ReducedDatum.objects.get_or_create(
                   timestamp=datetime.datetime.utcnow(),
                   value=json.dumps(data),
                   source_name='MOP',
@@ -79,22 +80,22 @@ class Command(BaseCommand):
                   data_type='lc_model',
                   target=target)
        
-           if created:
-              rd.save()
+               if created:
+                  rd.save()
 
-           time_now = Time(datetime.datetime.now()).jd
-           how_many_tE = (time_now-t0_fit)/tE_fit
+               time_now = Time(datetime.datetime.now()).jd
+               how_many_tE = (time_now-t0_fit)/tE_fit
 
 
-           if how_many_tE>2:
+               if how_many_tE>2:
 
-               alive = False
+                   alive = False
 
-           else:
+               else:
  
-               alive = True
+                   alive = True
 
-           extras = {'Alive':alive, 't0':np.around(t0_fit,3),'u0':np.around(u0_fit,5),'tE':np.around(tE_fit,3),
+               extras = {'Alive':alive, 't0':np.around(t0_fit,3),'u0':np.around(u0_fit,5),'tE':np.around(tE_fit,3),
                  'piEN':np.around(piEN_fit,5),'piEE':np.around(piEE_fit,5),
                  'Source_magnitude':np.around(mag_source_fit,3),
                  'Blend_magnitude':np.around(mag_blend_fit,3),
