105bfea1ae3ee22bbb54e2d4a300c8655aa2eb13
Etienne Bachelet <etibachelet@gmail.com>
2020-08-05 20:47:49 +0200

    Adding a cleaning data product command**6

--- mop/management/commands/remove_data_products.py
+++ mop/management/commands/remove_data_products.py
@@ -4,7 +4,7 @@ from tom_targets.models import Target
 from astropy.time import Time
 from mop.toolbox import fittools
 from mop.brokers import gaia as gaia_mop
-
+from django.conf import settings
 
 import json
 import numpy as np
@@ -16,10 +16,10 @@ class Command(BaseCommand):
     
     def add_arguments(self, parser):
         parser.add_argument('--targets_name', nargs='+', help='name of the events to clean')
-        parser.add_argument('--data_type', nargs='+', help='name of the data king to remove')
+        parser.add_argument('--data_type', nargs='+', help='name of the data kind to remove')
     
     def handle(self, *args, **options):
-       import pdb; pdb.set_trace()
+
        name = options['targets_name']
        data_type = options['data_type']
        
@@ -30,11 +30,11 @@ class Command(BaseCommand):
        
            list_of_targets = []
            for event in events:
-                if name[0] in i.name:
+                if name[0] in event.name:
                     list_of_targets.append(event)
                                  
        for target in list_of_targets:
            
-           ReducedDatum.objects.filter(target=target, data_type=settings.DATA_PRODUCT_TYPES[data_type][0]).delete()
+           ReducedDatum.objects.filter(target=target, data_type=settings.DATA_PRODUCT_TYPES[data_type[0]][0]).delete()
            print(target.name, ' : Clean!') 
 
