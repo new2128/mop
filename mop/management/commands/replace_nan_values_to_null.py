4b9748fd242a52bf442f9ef74888d34ea69dad65
Etienne Bachelet <etibachelet@gmail.com>
2021-04-06 03:48:45 +0200

    Fixing NaN in db**6

--- mop/management/commands/replace_nan_values_to_null.py
+++ mop/management/commands/replace_nan_values_to_null.py
@@ -9,6 +9,31 @@ from django.conf import settings
 import random
 import numpy as np
 import datetime
+import json
+
+def clean_lc_model(target):
+
+
+    try:
+    
+        existing_model =   ReducedDatum.objects.filter(source_location=target.name,data_type='lc_model',)
+        model = json.loads(existing_model[0].value)
+
+        if np.nan in json.loads(existing_model[0].value)['lc_model_magnitude']:
+
+            data = {'lc_model_time': [],
+                    'lc_model_magnitude': []}
+            
+            rd, created = ReducedDatum.objects.update_or_create(timestamp=existing_model[0].timestamp,
+                                                                value=json.dumps(data),
+                                                                source_name='MOP',
+                                                                source_location=target.name,
+                                                                data_type='lc_model',
+                                                                target=target)   
+            rd.save()         
+    except:
+    
+        pass
 
 class Command(BaseCommand):
 
@@ -45,5 +70,7 @@ class Command(BaseCommand):
                     pass
                     
             target.save(extras = extras)
+            clean_lc_model(target)
+            
             print(target.name, ' : Clean!') 
 
