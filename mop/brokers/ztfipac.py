7e6d8103b4970db1e9b3e6bf56d5af920f724caa
Etienne Bachelet <etibachelet@gmail.com>
2021-04-14 06:37:48 +0200

    Correct ZTF harvest**4

--- mop/brokers/ztfipac.py
+++ mop/brokers/ztfipac.py
@@ -50,87 +50,86 @@ class ZTFIPACBroker(GenericBroker):
 
         for index,event in enumerate(list_of_events):
 
+            
+            MARS_candidates = requests.get(list_of_mars_links[index]+'&format=json').json()
+            cone_search = str(MARS_candidates['results'][0]['candidate']['ra'])+','+str(MARS_candidates['results'][0]['candidate']['dec'])+','+str(0.0001)
+            mars_form = MARSQueryForm({'cone':cone_search,'query_name':'Query ZTF IPAC : '+event, 'broker':'MARS'})
+            mars_form.is_valid()
+            query = BrokerQuery.objects.create(
+                           name='Query ZTF IPAC : '+event,
+                           broker=mars.name,
+                           parameters=mars_form.cleaned_data
+                                           )
+            alerts = mars.fetch_alerts(query.parameters)
+            alerts = [*alerts]  
+
+            name = event
+            ra = np.median([alert['candidate']['ra'] for alert in alerts])
+            dec = np.median([alert['candidate']['dec'] for alert in alerts])
+            
             try:
-                MARS_candidates = requests.get(list_of_mars_links[index]+'&format=json').json()
-                cone_search = str(MARS_candidates['results'][0]['candidate']['ra'])+','+str(MARS_candidates['results'][0]['candidate']['dec'])+','+str(0.0001)
-                mars_form = MARSQueryForm({'cone':cone_search,'query_name':'Query ZTF IPAC : '+event, 'broker':'MARS'})
-                mars_form.is_valid()
-                query = BrokerQuery.objects.create(
-                               name='Query ZTF IPAC : '+event,
-                               broker=mars.name,
-                               parameters=parameters=mars_form.cleaned_data
-                                               )
-                alerts = mars.fetch_alerts(query.parameters)
-                alerts = [*alerts]  
-
-                name = event
-                ra = np.median([alert['candidate']['ra'] for alert in alerts])
-                dec = np.median([alert['candidate']['dec'] for alert in alerts])
-                
-                try:
-                
-                    target = Target.objects.get(name=name)
-                
-                except:
-                
-                    target, created =  Target.objects.get_or_create(name=name,ra=ra,dec=dec,type='SIDEREAL',epoch=2000)
-
-
-                    if created:
-
-                       target.save()
-                   
-
-                filters = {1: 'g_ZTF', 2: 'r_ZTF', 3: 'i_ZTF'}
-                try:
-                    times = [Time(i.timestamp).jd for i in ReducedDatum.objects.filter(target=target) if i.data_type == 'photometry']
-                except: 
-                    times = []
-
-                for alert in alerts:
-                
-                    if all([key in alert['candidate'] for key in ['jd', 'magpsf', 'fid', 'sigmapsf']]):
-                          jd = Time(alert['candidate']['jd'], format='jd', scale='utc')
-                          jd.to_datetime(timezone=TimezoneInfo())
-                          
-                          if alert['candidate']['isdiffpos'] == 't':
-                              signe = 1
-                          else:
-                              signe = -1
-                              
-                          flux = 10**(-0.4*alert['candidate']['magnr'])+signe*10**(-0.4*alert['candidate']['magpsf'])
-                          eflux = ((10**(-0.4*alert['candidate']['magnr'])*alert['candidate']['sigmagnr'])**2+(signe*10**(-0.4*alert['candidate']['magpsf'])*alert['candidate']['sigmapsf'])**2)**0.5
-                          
-                          mag = -2.5*np.log10(flux)
-                          emag = eflux/flux
-                          
-                          value = {
-                                   'magnitude': mag,
-                                   'filter': filters[alert['candidate']['fid']],
-                                   'error': emag
-                                   }
-                                   
-                          if  (jd.value not in times):
-                                    
-                                rd, _ = ReducedDatum.objects.get_or_create(
-                                        timestamp=jd.to_datetime(timezone=TimezoneInfo()),
-                                        value=value,
-                                        source_name='ZTF IPAC',
-                                        source_location='IRSA',
-                                        data_type='photometry',
-                                        target=target)
-                                
-                                rd.save()
-                          
-                          else:
-                          
-                                pass          
-                          
-                    else:
-
-                          pass
+            
+                target = Target.objects.get(name=name)
+            
             except:
-                  pass
+            
+                target, created =  Target.objects.get_or_create(name=name,ra=ra,dec=dec,type='SIDEREAL',epoch=2000)
+
+
+                if created:
+
+                   target.save()
+               
+
+            filters = {1: 'g_ZTF', 2: 'r_ZTF', 3: 'i_ZTF'}
+            try:
+                times = [Time(i.timestamp).jd for i in ReducedDatum.objects.filter(target=target) if i.data_type == 'photometry']
+            except: 
+                times = []
+
+            for alert in alerts:
+            
+                if all([key in alert['candidate'] for key in ['jd', 'magpsf', 'fid', 'sigmapsf']]):
+                      jd = Time(alert['candidate']['jd'], format='jd', scale='utc')
+                      jd.to_datetime(timezone=TimezoneInfo())
+                      
+                      if alert['candidate']['isdiffpos'] == 't':
+                          signe = 1
+                      else:
+                          signe = -1
+                          
+                      flux = 10**(-0.4*alert['candidate']['magnr'])+signe*10**(-0.4*alert['candidate']['magpsf'])
+                      eflux = ((10**(-0.4*alert['candidate']['magnr'])*alert['candidate']['sigmagnr'])**2+(signe*10**(-0.4*alert['candidate']['magpsf'])*alert['candidate']['sigmapsf'])**2)**0.5
+                      
+                      mag = -2.5*np.log10(flux)
+                      emag = eflux/flux
+                      
+                      value = {
+                               'magnitude': mag,
+                               'filter': filters[alert['candidate']['fid']],
+                               'error': emag
+                               }
+                               
+                      if  (jd.value not in times):
+                                
+                            rd, _ = ReducedDatum.objects.get_or_create(
+                                    timestamp=jd.to_datetime(timezone=TimezoneInfo()),
+                                    value=value,
+                                    source_name='ZTF IPAC',
+                                    source_location='IRSA',
+                                    data_type='photometry',
+                                    target=target)
+                            
+                            rd.save()
+                      
+                      else:
+                      
+                            pass          
+                      
+                else:
+
+                      pass
+           
 
 
     def to_generic_alert(self, alert):
