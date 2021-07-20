71addb9e043c16584d9e7bdcfe676853dea844be
new2128 <81988673+new2128@users.noreply.github.com>
2021-07-20 12:46:18 -0500

    Update harvest_asassn.py

--- mop/management/commands/harvest_asassn.py
+++ mop/management/commands/harvest_asassn.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from mop.asassn import ASASSNBroker
+from mop.brokers import asassn
 
 BROKER_URL = 'http://www.astronomy.ohio-state.edu/asassn/transients.html'
 photometry = 'https://asas-sn.osu.edu/photometry'
@@ -13,8 +13,8 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
 
-    def handle(self):
-        Asassn = ASASSNBroker()
+    def handle(self, *args, **options):
+        Asassn = asassn.ASASSNBroker('ASAS-SN Broker)
         table = Asassn.retrieve_transient_table()
         list_of_events = Asassn.retrieve_microlensing_coordinates(table)
         list_of_targets = Asassn.fetch_alerts(list_of_events)
